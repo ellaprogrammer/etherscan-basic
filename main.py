c49fc14a191f0714b2e7faab7fa3d33c90307021
Ella Queen <queenella55@gmail.com>
2022-07-02 04:50:59 +0000

    store transactions and their output in db

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 import traceback
 
 import flask
-# from replit import db
+from replit import db
 
 EMOJI = ['💯', '❤️', '🥳', '💩']
 my_secret = os.environ['api_key']
@@ -14,6 +14,11 @@ app = flask.Flask(__name__)
 def internal_server_error(e: str):
   return flask.jsonify(error=str(e)), 500
 
+# how to route to diff endpoint?
+@app.route('/db', methods=['GET, POST'])
+def info():
+  for key in db.keys():
+    print(key)
 
 @app.route('/', methods=['GET', 'POST'])
 def comments():
@@ -21,7 +26,17 @@ def comments():
     # counts = db.get('emoji', {})
     output_addresses = ['']
     if flask.request.method == "POST" and 'transaction' in flask.request.form:
-      output_addresses = [flask.request.form['transaction']]
+      transaction_hash = [flask.request.form['transaction']]
+      transaction_matches = ['address_requested_found1,address_requested_found2']
+      # output_addresses = output of API call
+      output_addresses = transaction_matches
+
+      # join input list to store in DB as key string
+      joined_string = ",".join(transaction_hash)
+      # let's prevent against scammers clogging out DB
+      if len(joined_string) > 70:
+        joined_string = joined_string[:70] + '_ABBREV'  
+      db[joined_string] = output_addresses
     # if flask.request.method == "POST" and 'emoji' in flask.request.form:
     #   emoji = flask.request.form['emoji']
     #   if emoji in EMOJI:
